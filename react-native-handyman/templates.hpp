68d9799b0ca342092f71810a3a82068d860f82b1
Tyler Williams <tyler@coolsoftware.dev>
2024-10-20 13:16:29 -0600

    feat: add podspec template

--- react-native-handyman/templates.hpp
+++ react-native-handyman/templates.hpp
@@ -73,3 +73,25 @@ export default codegenNativeComponent<{{componentName}}Props>('{{componentName}}
 }
 )";
 }
+
+const std::string PODSPEC = R"(
+require "json"
+
+package = JSON.parse(File.read(File.join(__dir__, "package.json")))
+
+Pod::Spec.new do |s|
+  s.name            = "{{componentName}}"
+  s.version         = package["version"]
+  s.summary         = package["description"]
+  s.description     = package["description"]
+  s.homepage        = package["homepage"]
+  s.license         = package["license"]
+  s.platforms       = { :ios => "11.0" }
+  s.author          = package["author"]
+  s.source          = { :git => package["repository"], :tag => "#{s.version}" }
+
+  s.source_files    = "ios/**/*.{h,m,mm,swift}"
+
+  install_modules_dependencies(s)
+end
+)";
